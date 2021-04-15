f8d34d3553127a2914f25be0da809eae8928bc0b
mandatoryrecess <rossanneclamp@gmail.com>
2021-04-15 00:37:06 +0000

    odd.js file assignement

---
+++ odd.js
@@ -0,0 +1,10 @@
+const isOdd  = function (num) {
+  return num % 2 === 1;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("91 is odd: " + isOdd(91));
+console.log("13 is odd: " + isOdd(13));
+console.log("20 is odd: " + isOdd(20));
+console.log("44 is odd: " + isOdd(44));
