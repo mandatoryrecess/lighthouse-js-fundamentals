d38b5009d97d2bbfdfc42487d817228ed2c16e20
mandatoryrecess <rossanneclamp@gmail.com>
2021-04-16 17:22:51 +0000

    added comments

--- ingredients.js
+++ ingredients.js
@@ -1,5 +1,6 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
+// Write a while loop
 let i = 0;
 while (i < ingredients.length) {
   console.log(ingredients[i]);
